28cb586797f04431a78a3cfe190a416aa8745755
PADMANABHAN <padhusenthil2006@gmail.com>
2026-03-25 00:44:03 +0530

    Time: 1 ms (99.96%), Space: 42.6 MB (87.45%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,12 +1,20 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int ind=1;
-       for(int i=1;i<=n;i++){
-            if(n>0){
-            n-=i;
-            ind=i;
+        //trying binary search
+        long start=1;
+        long end=n;
+        int res=0;
+        while(start<=end){
+            long mid=start+(end-start)/2;
+            long coins_needed=mid*(mid+1)/2;
+            if(n>=coins_needed){
+                res=(int)mid;
+                start=mid+1;
             }
-       }
-       return ind;
+            else{
+                end=mid-1;
+            }
+        }
+        return res;
     }
 }
